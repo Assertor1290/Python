548f7808bbad22b7f4b017da8cfa03d2c90c783c
Kushagra Kumar <jakraso201@gmail.com>
2021-05-31 22:43:52 +0530

    Lambda function

--- 14_Lamda.py
+++ 14_Lamda.py
@@ -13,3 +13,25 @@ print(sum(5,6))
 # Immediately Invoked Function Expression (IIFE, pronounced “iffy”)
 print((lambda x, y: x + y)(2, 3))
 
+# Lambda with higher order functions
+
+# 1. FILTER: Filter out elements from iterable
+# according to condition
+my_list = [2, 3, 4, 5, 6, 7, 8, 9]
+# function to filter even numbers
+new_list_filter = filter(lambda x: (x % 2 == 0), my_list)
+# <filter object at 0x00000254DEDB94A8>
+print(new_list_filter)
+# class 'filter'
+print(type(new_list_filter))
+new_list = list(filter(lambda x: (x % 2 == 0), my_list))
+print(new_list)
+
+# 2. MAP: Applies function to all elements of iterables
+my_list = [2, 3, 4, 5, 6, 7, 8, 9]
+new_list = list(map(lambda x: x * x, my_list))
+print(new_list)
+
+my_list = [2,3,4,5,6,7,8]
+new_list = list(map(lambda a: (a/3 != 2), my_list))
+print(new_list)
