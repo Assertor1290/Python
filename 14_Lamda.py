0c9f21227350235ed24a6c9019748458c03ee146
Kushagra Kumar <jakraso201@gmail.com>
2021-05-31 23:07:52 +0530

    Lambda function

--- 14_Lamda.py
+++ 14_Lamda.py
@@ -35,3 +35,7 @@ print(new_list)
 my_list = [2,3,4,5,6,7,8]
 new_list = list(map(lambda a: (a/3 != 2), my_list))
 print(new_list)
+
+# 3. REDUCE
+from functools import reduce
+print(reduce(lambda a,b: a+b, [23,21,45,98]))
